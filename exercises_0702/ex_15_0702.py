a187313aaa9dd4bf4a29555d768ec42742fd6f01
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-08-07 22:50:49 -0300

    Create ex_15_0702.py

---
+++ exercises_0702/ex_15_0702.py
@@ -0,0 +1,5 @@
+"""
+Leia uma matriz 5x10 que se refere  respostas de 10 questões de multipla escolha, referentes a 5 alunos. Leia também um vetor de 10 posições contendo o gabarito de
+respostas que podem ser a, b, c ou d. Seu programa deverá comparar as respsotas de cada candidato com o gabarito e emitir um vetor denominado resultado, contendo a 
+pontução correspondente a cada aluno.
+"""
