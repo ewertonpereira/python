333d34e5cb59bcc0deef37cd9476402cd7b4f453
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-08-10 12:51:57 -0300

    refactored quiz2.0.py

--- quiz.2.0.py
+++ quiz.2.0.py
@@ -1,129 +1,197 @@
+"""
+O algoritmo abaixo foi desenvolvido com o intuito de representar uma prova múltipla escolha.
+
+Características:
+    - Randomizar a ordem de exibição das questões a cada execução;
+    - Randomizar a ordem de exibição das alternativas de cada questão a cada execução;
+    - Receber a opção da alternativa escolhida pelo usuário;
+    - Possibilida ao usuário sair do programa a qualquer momento;
+    - Checar e mostrar a resposta correta;
+    - Mostrar o percentual de acertos do usuário
+    - Possibilita o usuário tentar o questionário novamente
+"""
 from random import shuffle
 
-question_number: int = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10]
-shuffle(question_number)
-lista_escolhas = ['a', 'b', 'c', 'd']
-shuffle(lista_escolhas)
+def quiz() -> None:
 
-perguntas = {
-    question_number[0]: {
-        'pergunta': 'Quanto é 2 + 2?',
-        'alternativas': {
-            lista_escolhas[0]: '0',
-            lista_escolhas[1]: '1',
-            lista_escolhas[2]: '2',
-            lista_escolhas[3]: '4'
-        },
-        'resposta_certa': lista_escolhas[3]
-    },
-    question_number[1]: {
-        'pergunta': 'Quem descobriu a américa?',
-        'alternativas': {
-            lista_escolhas[0]: 'Pedro Álvares Cabral',
-            lista_escolhas[1]: 'Dom Pedro I',
-            lista_escolhas[2]: 'Cristóvão Colombo',
-            lista_escolhas[3]: 'Bartolomeu Dias'
+    answer: bool = False
+    user_answer: str = ''
 
-        },
-        'resposta_certa': lista_escolhas[0]
-    },
-    question_number[2]: {
-        'pergunta': 'Qual o nome da galáxia em que está o sistema solar?',
-        'alternativas': {
-            lista_escolhas[0]: 'Redemoinho',
-            lista_escolhas[1]: 'Sombreiro',
-            lista_escolhas[2]: 'Andrômeda',
-            lista_escolhas[3]: 'Via Láctea'
-        },
-        'resposta_certa': lista_escolhas[3]
-    },
-    question_number[3]: {
-        'pergunta': 'Quem foi o lateral direito titular do Brasil na copa do mundo 2002?',
-        'alternativas': {
-            lista_escolhas[0]: 'Belleti',
-            lista_escolhas[1]: 'Maicon',
-            lista_escolhas[2]: 'Cafu',
-            lista_escolhas[3]: 'Daniel Alves'
-        },
-        'resposta_certa': lista_escolhas[2]
-    },
-    question_number[4]: {
-        'pergunta': 'Quanto é 3 + 5 * 2',
-        'alternativas': {
-            lista_escolhas[0]: '13',
-            lista_escolhas[1]: '10',
-            lista_escolhas[2]: '16',
-            lista_escolhas[3]: '17'
-        },
-        'resposta_certa': lista_escolhas[0]
-    },
-    question_number[5]: {
-        'pergunta': 'O tomate é?',
-        'alternativas': {
-            lista_escolhas[0]: 'Uma leguminosa',
-            lista_escolhas[1]: 'Uma fruta',
-            lista_escolhas[2]: 'Um tubérculo',
-            lista_escolhas[3]: 'Um vegetal'
-        },
-        'resposta_certa': lista_escolhas[1]
-    },
-    question_number[6]: {
-        'pergunta': 'Qual desses animais não é um mamífero?',
-        'alternativas': {
-            lista_escolhas[0]: 'Tubarão',
-            lista_escolhas[1]: 'Ornitorrinco',
-            lista_escolhas[2]: 'Tamanduá',
-            lista_escolhas[3]: 'Hipopótamo'
-        },
-        'resposta_certa': lista_escolhas[0]
-    },
-    question_number[7]: {
-        'pergunta': 'Quantos olhos tem um ciclope na mitologia grega?',
-        'alternativas': {
-            lista_escolhas[0]: '4',
-            lista_escolhas[1]: '3',
-            lista_escolhas[2]: '2',
-            lista_escolhas[3]: '1'
-        },
-        'resposta_certa': lista_escolhas[3]
-    },
-    question_number[8]: {
-        'pergunta': 'Quantos estados possui a regição sul do Brasil?',
-        'alternativas': {
-            lista_escolhas[0]: '4',
-            lista_escolhas[1]: '3',
-            lista_escolhas[2]: '2',
-            lista_escolhas[3]: '1'
-        },
-        'resposta_certa': lista_escolhas[1]
-    },
-    question_number[9]: {
-        'pergunta': 'As _______ são responsáveis pela produção de energia para todo o orgânismo.',
-        'alternativas': {
-            lista_escolhas[0]: 'Organélas',
-            lista_escolhas[1]: 'Mitocôndrias',
-            lista_escolhas[2]: 'Bactérias',
-            lista_escolhas[3]: 'Companias elétricas'
-        },
-        'resposta_certa': lista_escolhas[1]
-    },
-}
-correct_answers: int = 0
+    print('=' * 90)
+    print(title := 'MENU PRINCIPAL'.center(90))
+    print('=' * 90)
 
-for question_key, question_value in sorted(perguntas.items()):
-        shuffle(lista_escolhas)
-        print(f'{question_key} - {question_value["pergunta"]}')
-        for answer_key , answer_value in sorted(question_value['alternativas'].items()):
-            print(f'{answer_key} - {answer_value}')
-
-        user_answers: str = input('\nDigite sua escolha: ')
-
-        if user_answers == question_value['resposta_certa']:
-            print('\nVocê acertou!')
-            correct_answers += 1
-        else:
-            print(f'\nVocê errou! A resposta certa é : {question_value["resposta_certa"]}')
-        print('')
+    while not answer:
+        user_answer: str = input('\nDigite [1] para começar o questionário ou [0] para sair: ')
+        if user_answer == '0' or user_answer == '1':
+            answer = True
 
 
-print(f'Percentual de acerto: {(resul := (correct_answers / (number_questions := (len(perguntas))) * 100))} %')
+    match user_answer:
+        case '1':
+
+            print('')
+            print('=' * 90)
+            print(title := 'QUESTIONÁRIO'.center(90))
+            print('=' * 90)
+            question_number: int = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10]
+            shuffle(question_number)
+            list_choices:list[str] = ['a', 'b', 'c', 'd']
+            shuffle(list_choices)
+
+            questions = {
+                question_number[0]: {
+                    'question': 'Quanto é 2 + 2?',
+                    'alternatives': {
+                        list_choices[0]: '0',
+                        list_choices[1]: '1',
+                        list_choices[2]: '2',
+                        list_choices[3]: '4'
+                    },
+                    'correct_answer': list_choices[3]
+                },
+                question_number[1]: {
+                    'question': 'Quem descobriu a América?',
+                    'alternatives': {
+                        list_choices[0]: 'Pedro Álvares Cabral',
+                        list_choices[1]: 'Dom Pedro I',
+                        list_choices[2]: 'Cristóvão Colombo',
+                        list_choices[3]: 'Bartolomeu Dias'
+
+                    },
+                    'correct_answer': list_choices[0]
+                },
+                question_number[2]: {
+                    'question': 'Qual o nome da galáxia em que está o sistema solar?',
+                    'alternatives': {
+                        list_choices[0]: 'Redemoinho',
+                        list_choices[1]: 'Sombreiro',
+                        list_choices[2]: 'Andrômeda',
+                        list_choices[3]: 'Via Láctea'
+                    },
+                    'correct_answer': list_choices[3]
+                },
+                question_number[3]: {
+                    'question': 'Quem foi o lateral direito titular do Brasil na copa do mundo 2002?',
+                    'alternatives': {
+                        list_choices[0]: 'Belleti',
+                        list_choices[1]: 'Maicon',
+                        list_choices[2]: 'Cafu',
+                        list_choices[3]: 'Daniel Alves'
+                    },
+                    'correct_answer': list_choices[2]
+                },
+                question_number[4]: {
+                    'question': 'Quanto é 3 + 5 * 2',
+                    'alternatives': {
+                        list_choices[0]: '13',
+                        list_choices[1]: '10',
+                        list_choices[2]: '16',
+                        list_choices[3]: '17'
+                    },
+                    'correct_answer': list_choices[0]
+                },
+                question_number[5]: {
+                    'question': 'O tomate é?',
+                    'alternatives': {
+                        list_choices[0]: 'Uma leguminosa',
+                        list_choices[1]: 'Uma fruta',
+                        list_choices[2]: 'Um tubérculo',
+                        list_choices[3]: 'Um vegetal'
+                    },
+                    'correct_answer': list_choices[1]
+                },
+                question_number[6]: {
+                    'question': 'Qual desses animais não é um mamífero?',
+                    'alternatives': {
+                        list_choices[0]: 'Tubarão',
+                        list_choices[1]: 'Ornitorrinco',
+                        list_choices[2]: 'Tamanduá',
+                        list_choices[3]: 'Hipopótamo'
+                    },
+                    'correct_answer': list_choices[0]
+                },
+                question_number[7]: {
+                    'question': 'Quantos olhos tem um ciclope na mitologia grega?',
+                    'alternatives': {
+                        list_choices[0]: '4',
+                        list_choices[1]: '3',
+                        list_choices[2]: '2',
+                        list_choices[3]: '1'
+                    },
+                    'correct_answer': list_choices[3]
+                },
+                question_number[8]: {
+                    'question': 'Quantos estados possui a regi1ão sul do Brasil?',
+                    'alternatives': {
+                        list_choices[0]: '4',
+                        list_choices[1]: '3',
+                        list_choices[2]: '2',
+                        list_choices[3]: '1'
+                    },
+                    'correct_answer': list_choices[1]
+                },
+                question_number[9]: {
+                    'question': 'As _______ são responsáveis pela produção de energia para todo o orgânismo.',
+                    'alternatives': {
+                        list_choices[0]: 'Organélas',
+                        list_choices[1]: 'Mitocôndrias',
+                        list_choices[2]: 'Bactérias',
+                        list_choices[3]: 'Companias elétricas'
+                    },
+                    'correct_answer': list_choices[1]
+                },
+            }
+            correct_answers: int = 0
+
+            for question_key, question_value in sorted(questions.items()):
+                    shuffle(list_choices)
+                    print(f'{question_key} - {question_value["question"]}')
+                    for answer_key , answer_value in sorted(question_value['alternatives'].items()):
+                        print(f'{answer_key} - {answer_value}')
+
+                    user_answers: str = input('\nDigite uma das alternativas ou digite [s] para sair: ').lower()
+                    if user_answers == 's':
+                            exit()
+
+                    while user_answers != 'a' and user_answers != 'b' and user_answers != 'c' and user_answers != 'd' and user_answers != 's':
+                        user_answers: str = input('\nDigite uma das alternativas [a - b - c - d] ou digite [s] para sair: ').lower()
+                        if user_answers == 's':
+                            exit()
+
+
+                    if user_answers == question_value['correct_answer']:
+                        print('\nVocê acertou!')
+                        correct_answers += 1
+                    else:
+                        print(f'\nVocê errou! A resposta certa é : {question_value["correct_answer"]}')
+                    print('')
+
+
+            print(f'Percentual de acerto: {(resul := (correct_answers / (number_questions := (len(questions))) * 100))} %')
+            
+            print('=' * 90)
+            print(title := 'Created by Ewerton Pereira'.center(90))
+            print('=' * 90)
+
+            answer = False
+
+            while not answer:
+                user_answer: str = input('\nDigite [1] se deseja reiniciar o questionário ou [0] para sair: ')
+                if user_answer == '0' or user_answer == '1':
+                    answer = True
+
+
+            match user_answer:
+                case '1':
+                    quiz()
+                case '2':
+                    exit()
+
+
+        case '2':
+            exit()
+
+
+if __name__ == '__main__':
+    quiz()
