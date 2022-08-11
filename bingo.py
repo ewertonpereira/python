096b2762bbf457bef7ab116af46e68196aa83eaa
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-08-11 16:09:10 -0300

    Update bingo.py

--- bingo.py
+++ bingo.py
@@ -1,5 +1,8 @@
 from random import randint, shuffle
 
+# emojis
+# https://unicode.org/emoji/charts/full-emoji-list.html
+
 # create a matrix of matrices
 def create_array_matrices(size_matrix: int) -> list[list]:
     matrix: list[list] = []
@@ -25,51 +28,61 @@ def add_numbers_list(min_rand: int, max_rand: int) -> list[int]:
 
     return array
 
+# 
+def create_bingo_card(matrix: list) -> list[list[int]]:
 
-# Shows matrix's values
+    matrix_size:int = 5
+    bingo_card: list[list[int]] = create_array_matrices(matrix_size)
+    
+    for row in range(len(bingo_card)):
+        for column in range(len(bingo_card)):
+            if row == 2 and column == 2:
+                bingo_card[row].append('\U0001F505')
+            elif column == 0:
+                shuffle(matrix[0])
+                bingo_card[row].append(element := matrix[0].pop())
+            elif column == 1:
+                shuffle(matrix[1])
+                bingo_card[row].append(element := matrix[1].pop())
+            elif column == 2:
+                shuffle(matrix[2])
+                bingo_card[row].append(element := matrix[2].pop())
+            elif column == 3:
+                shuffle(matrix[3])
+                bingo_card[row].append(element := matrix[3].pop())
+            elif column == 4:
+                shuffle(matrix[4])
+                bingo_card[row].append(element := matrix[4].pop())
+                
+
+    return bingo_card
+
+
+# Shows bingo's matrix values
 def show_matrix(matrix: list) -> None:
+    print('=' * 29)
+    print(title := 'BINGO'.center(29))
+    print('=' * 29)
     for row in range(len(matrix)):
         for column in range(len(matrix)):
             print(f'{matrix[row][column]:^6}', end='')
         print('')
 
 
-b: list = add_numbers_list(1, 15)
-i: list = add_numbers_list(16, 30)
-n: list = add_numbers_list(31, 45)
-g: list = add_numbers_list(46, 60)
-o: list = add_numbers_list(61, 75)
+    print('=' * 29)
 
-bingox: list[list[int]] = [b, i, n, g, o]
 
-def bingo(matrix: list) -> list[list[int]]:
+if __name__ == '__main__': 
 
-    matrix_size:int = 5
-    bingo_matrix: list[list[int]] = create_array_matrices(matrix_size)
+    b: list = add_numbers_list(1, 15)
+    i: list = add_numbers_list(16, 30)
+    n: list = add_numbers_list(31, 45)
+    g: list = add_numbers_list(46, 60)
+    o: list = add_numbers_list(61, 75)
+
+    bingo: list[list[int]] = [b, i, n, g, o]
+    bingo_card: list[list[int]] = create_bingo_card(bingo)
+
+    show_matrix(bingo_card)
+
     
-    for row in range(len(bingo_matrix)):
-        for column in range(len(bingo_matrix)):
-            if row == 2 and column == 2:
-                bingo_matrix[row].append('x')
-            elif column == 0:
-                shuffle(matrix[0])
-                bingo_matrix[row].append(element := matrix[0].pop())
-            elif column == 1:
-                shuffle(matrix[1])
-                bingo_matrix[row].append(element := matrix[1].pop())
-            elif column == 2:
-                shuffle(matrix[2])
-                bingo_matrix[row].append(element := matrix[2].pop())
-            elif column == 3:
-                shuffle(matrix[3])
-                bingo_matrix[row].append(element := matrix[3].pop())
-            elif column == 4:
-                shuffle(matrix[4])
-                bingo_matrix[row].append(element := matrix[4].pop())
-
-
-    return bingo_matrix
-
-
-#print(bingo(bingox))
-print(show_matrix(bingo(bingox)))
