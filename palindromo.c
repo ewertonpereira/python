9e8963e4351374cefc24a9b9d18e25ba9413932b
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-09-09 12:19:59 -0300

    test c

---
+++ palindromo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (){
+
+    char word[50];
+    char copy_word[50];
+    int size, i, j;
+
+    printf("Digite uma palavra: ");
+    fgets(word, 50, stdin);
+    strcpy(copy_word, word);
+    size = strlen(word);
+
+    j = size - 2;
+
+    for(i = 0; i < (size - 2); i++){
+        if(word[i] != copy_word[j]){
+            printf("Nao eh um palindromo.");
+            return 0;
+        }
+        j--;    
+    }
+
+    printf("Eh palindromo!\n\n");
+    // printf("Size: %d\nWord: %s\nCopy word: %s", size, word, copy_word);
+
+    return 0;
+}
