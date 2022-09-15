14e1ecf64d4a3a4507d8a4081e7b6bdf05ba065c
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-09-15 12:37:18 -0300

    Update test2.py

---
+++ test2.py
@@ -0,0 +1,11 @@
+"""
+    - se a função somar_dois_numeros existe
+    - passar dois números como parâmetro
+    - testar resultado da função
+"""
+def test_testar_parametros_da_funcao_somar_dois_numeros():
+    somar_dois_numeros(1, 2)
+
+
+def somar_dois_numeros(num1, num2):
+    pass
