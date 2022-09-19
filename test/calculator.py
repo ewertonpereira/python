6bb74a7960363d4206c0a78d6884c0ff50e90c8a
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-09-18 22:06:56 -0300

    Update calculator.py

--- test/calculator.py
+++ test/calculator.py
@@ -2,8 +2,12 @@ from numbers import Number
 
 
 class Calculator():
+
     def addition(self, number1, number2):
         if isinstance(number1, Number) and isinstance(number2, Number):
             return number1 + number2
         else:
             raise Exception('Insira somente números.')
+            
+def subtraction():
+    pass
