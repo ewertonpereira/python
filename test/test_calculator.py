43600f566a7e99bb5b59e335282a32030ab3dde1
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-09-19 10:04:34 -0300

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import result
 from calculator  import Calculator
 
 
@@ -9,12 +10,12 @@ class TestCalculatorAddition(unittest.TestCase):
 
 
     def test_addition_two_positive_numbers(self):
-        result = self.calc.sum(2, 2)
+        result = self.calc.addition(2, 2)
         self.assertEqual(result, 4)
 
 
     def test_addition_two_negative_numbers(self):
-        result = self.calc.sum(-2, -2)
+        result = self.calc.addition(-2, -2)
         self.assertEqual(result, -4)
 
 
@@ -24,18 +25,29 @@ class TestCalculatorAddition(unittest.TestCase):
 
 
     def test_addition_two_float_numbers(self):
-        result = self.calc.sum(2.0, 2.0)
+        result = self.calc.addition(2.0, 2.0)
         self.assertEqual(result, 4.0)
 
 
     def test_addition_number_string(self):
         with self.assertRaises(Exception):
-            self.calc.sum(2, 'b')
+            self.calc.addition(2, 'b')
 
 
     def test_addition_string_number(self):
         with self.assertRaises(Exception):
-            self.calc.sum('a', 2)
+            self.calc.addition('a', 2)
+
+
+class TestClaculatorSubtraction(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calc = Calculator()
+
+    
+    def test_subtraction_two_positive_numbers(self):
+        result = self.calc.subtraction(2, 1)
+        self.assertEqual(result, 1)
 
 
 if __name__ == '__main__':
