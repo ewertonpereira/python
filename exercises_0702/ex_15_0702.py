97684d1d6e0e9499b6ce4c3189cb44d606237385
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-09-27 15:35:00 -0300

    get answer 15-07-02

--- exercises_0702/ex_15_0702.py
+++ exercises_0702/ex_15_0702.py
@@ -3,3 +3,25 @@ Leia uma matriz 5x10 que se refere  respostas de 10 questões de multipla escolh
 respostas que podem ser a, b, c ou d. Seu programa deverá comparar as respsotas de cada candidato com o gabarito e emitir um vetor denominado resultado, contendo a 
 pontução correspondente a cada aluno.
 """
+def check_answer(answer: str, question: int):
+    if isinstance(answer, str):
+        if answer !='A' or answer !='B' or answer !='C' or answer !='D':
+            answer: str = get_answer(question)
+        else:
+            return answer
+    else:
+        answer: str = get_answer(question)
+
+
+def get_answer(question: int):
+    answer: str = input(f'Digite a resposta da questão {question}: ').upper()
+    check_answer(answer)
+    
+
+
+def answers():
+
+
+word = 'boi'
+word = isinstance(word, str)
+print(word)
