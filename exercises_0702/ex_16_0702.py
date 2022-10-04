0bf96add3a3d5bc72a187dc0d9100fcf0ae4ee04
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-04 15:42:41 -0300

    Create ex_16_0702.py

---
+++ exercises_0702/ex_16_0702.py
@@ -0,0 +1,3 @@
+"""
+Faça um programa para corrigir uma prova de 10 questões multipla escolha (a, b, c, d, e), em uma turma com 3 alunos. Cada questão vale 1 ponto. Leia o gabarito, e para cada aluno leia sua matrícula(número inteiro) e suas respostas e sua nota. O percentual de aprovação, assumindo média 7,0.
+"""
