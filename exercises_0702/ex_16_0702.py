998dbaa79a6771915bea81d1dfd70d2ca707e1b4
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-04 16:20:24 -0300

    get_students_amount

--- exercises_0702/ex_16_0702.py
+++ exercises_0702/ex_16_0702.py
@@ -1,3 +1,28 @@
 """
 Faça um programa para corrigir uma prova de 10 questões multipla escolha (a, b, c, d, e), em uma turma com 3 alunos. Cada questão vale 1 ponto. Leia o gabarito, e para cada aluno leia sua matrícula(número inteiro) e suas respostas e sua nota. O percentual de aprovação, assumindo média 7,0.
 """
+from os import system
+from numbers import Number
+
+
+
+# clean terminal screen
+def clear():
+    return system('cls')
+
+
+def get_students_amount() -> int:
+
+    try:
+        students_amount: int = int(input('Digite a quantidade de alunos: '))
+        if isinstance(students_amount, int):
+                return students_amount
+    except ValueError:
+        clear()
+        print('Digite apenas números inteiros!\n')
+        students_amount: int = get_students_amount()
+
+        return students_amount
+
+
+print(number := (get_students_amount()))
