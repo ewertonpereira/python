e9a4eea871f28d7b31c2024c8fbd27f8f5bf1e5d
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-07 15:12:06 -0300

    Update _app.tsx

--- react/frontend/pages/_app.tsx
+++ react/frontend/pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../src/components/Header/Header'
+import { ThemeProvider } from '@mui/material'
+import tema from '../src/themes/theme'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={tema}>
       <Header></Header>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
