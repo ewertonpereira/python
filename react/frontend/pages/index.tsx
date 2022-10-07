69160f3c561383cf496dc2e3916082fd9850b266
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-07 16:05:12 -0300

    Update index.tsx

--- react/frontend/pages/index.tsx
+++ react/frontend/pages/index.tsx
@@ -1,10 +1,45 @@
+import { Box } from '@mui/material'
 import type { NextPage } from 'next'
+import Person from '../src/@types/person';
 import List from '../src/components/List/List'
 
 
 const Home: NextPage = () => {
+  const people: Person[] = [
+    {
+      id: 1,
+      name: 'Ewerton 1',
+      picture: "https://github.com/ewertonpereira.png",
+      description: 'O cara mais foda que você irá conhecer!',
+      hour_value: 100
+    },
+    {
+      id: 2,
+      name: 'Ewerton 2',
+      picture: "https://github.com/ewertonpereira.png",
+      description: 'O cara mais foda que você irá conhecer!',
+      hour_value: 100
+    },
+    {
+      id: 3,
+      name: 'Ewerton 3',
+      picture: "https://github.com/ewertonpereira.png",
+      description: 'O cara mais foda que você irá conhecer!',
+      hour_value: 100
+    },
+    {
+      id: 4,
+      name: 'Ewerton 4',
+      picture: "https://github.com/ewertonpereira.png",
+      description: 'O cara mais foda que você irá conhecer!',
+      hour_value: 100
+    },
+  ]
+
   return (
-    <List></List>
+    <Box sx={{backgroundColor: 'secondary.main'}}>
+      <List people={people}></List>
+    </Box>
   )
 }
 
