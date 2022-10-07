6332011adb9bd6eacc512fff1a7c10dc6ab9bd18
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-07 13:47:14 -0300

    Update _app.tsx

--- react/frontend/pages/_app.tsx
+++ react/frontend/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../src/components/Header/Header'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
