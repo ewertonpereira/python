a4fdeab9f25d2843435ed9a51386795b42e74ac2
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-07 16:05:14 -0300

    Update List.tsx

--- react/frontend/src/components/List/List.tsx
+++ react/frontend/src/components/List/List.tsx
@@ -1,30 +1,32 @@
 import { Button } from "@mui/material";
-import { Description, Information, ItemList, ListStyled, Name, Picture, Value } from "./List.style";
+import Person from "../../@types/person";
+import { Description, EmptyList, Information, ItemList, ListStyled, Name, Picture, Value } from "./List.style";
 
-const List = () => {
+interface ListProps{
+    people: Person[],
+}
+
+const List = (props: ListProps) => {
     return  (
-        <ListStyled>
-            <ItemList>
-                <Picture src="https://github.com/ewertonpereira.png"></Picture>
-                <Information>
-                    <Name>Ewerton Pereira</Name>
-                    <Value>R$ 100,00 por hora</Value>
-                    <Description>O cara mais foda que você irá conhecer!</Description>
-
-                    <Button>Marcar encontro</Button>
-                </Information>
-            </ItemList>
-            <ItemList>
-                <Picture src="https://github.com/ewertonpereira.png"></Picture>
-                <Information>
-                    <Name>Ewerton Pereira</Name>
-                    <Value>R$ 100,00 por hora</Value>
-                    <Description>O cara mais foda que você irá conhecer!</Description>
-
-                    <Button>Marcar encontro</Button>
-                </Information>
-            </ItemList>
-        </ListStyled>
+        <div>
+            {props.people.length > 0 ? (
+                <ListStyled>
+                {props.people.map(person => (
+                    <ItemList key={person.id}>
+                    <Picture src={person.picture}></Picture>
+                    <Information>
+                        <Name>{person.name}</Name>
+                        <Value>{person.hour_value.toLocaleString('pt-BR', {minimumFractionDigits: 2, style: 'currency', currency: 'BRL'})} por hora</Value>
+                        <Description>{person.description}</Description>
+                        <Button sx={{width: '70%'}}>Marcar encontro com {person.name}</Button>
+                    </Information>
+                </ItemList>
+                ))}
+            </ListStyled>
+            ) : (
+                <EmptyList>Nenhum item encontrado</EmptyList>
+            )}
+        </div>
     )
 }
 
