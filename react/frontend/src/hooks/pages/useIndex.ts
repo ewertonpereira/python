7a69f621a1fe1e915be21c7f6a56824eac6b0f05
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-14 21:16:41 -0300

    Update useIndex.ts

--- react/frontend/src/hooks/pages/useIndex.ts
+++ react/frontend/src/hooks/pages/useIndex.ts
@@ -1,39 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Person from "../../@types/person";
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex(){
-    const [listPeople, setListPeople] = useState<Person[]>(
-        [
-            {
-              id: 1,
-              name: 'Ewerton 1',
-              picture: "https://github.com/ewertonpereira.png",
-              description: 'O cara mais foda que você irá conhecer!',
-              hour_value: 100
-            },
-            {
-              id: 2,
-              name: 'Ewerton 2',
-              picture: "https://github.com/ewertonpereira.png",
-              description: 'O cara mais foda que você irá conhecer!',
-              hour_value: 100
-            },
-            {
-              id: 3,
-              name: 'Ewerton 3',
-              picture: "https://github.com/ewertonpereira.png",
-              description: 'O cara mais foda que você irá conhecer!',
-              hour_value: 100
-            },
-            {
-              id: 4,
-              name: 'Ewerton 4',
-              picture: "https://github.com/ewertonpereira.png",
-              description: 'O cara mais foda que você irá conhecer!',
-              hour_value: 100
-            },
-        ]
-    );
+    const [listPeople, setListPeople] = useState<Person[]>([]);
+
+    useEffect(() => {
+        ApiService.get('/people').then((answer) => {
+            setListPeople(answer.data)
+        })
+    }), [];
+
     return {
         listPeople
     }
