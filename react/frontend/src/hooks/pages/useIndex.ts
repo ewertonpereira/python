27a1c17480a89a0297f48eddc597f843d7cd2c46
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-15 22:52:16 -0300

    Update useIndex.ts

--- react/frontend/src/hooks/pages/useIndex.ts
+++ react/frontend/src/hooks/pages/useIndex.ts
@@ -5,6 +5,7 @@ import { ApiService } from "../../services/ApiService";
 export function useIndex(){
     const [listPeople, setListPeople] = useState<Person[]>([]);
 
+    
     useEffect(() => {
         ApiService.get('/people').then((answer) => {
             setListPeople(answer.data)
