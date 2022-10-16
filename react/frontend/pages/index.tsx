21c1744d65c4327d398337c8bcaabb91ea32bebd
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-16 20:58:15 -0300

    Update index.tsx

--- react/frontend/pages/index.tsx
+++ react/frontend/pages/index.tsx
@@ -1,16 +1,46 @@
-import { Box } from '@mui/material'
+import { Box, Button, Dialog, DialogActions, Grid, TextField } from '@mui/material'
 import type { NextPage } from 'next'
 import List from '../src/components/List/List'
 import { useIndex } from '../src/hooks/pages/useIndex';
 
 
 const Home: NextPage = () => {
-  const  { listPeople } = useIndex();
+  const  { listPeople, name, setName, email, setEmail } = useIndex();
 
   return (
-    <Box sx={{backgroundColor: 'secondary.main'}}>
-      <List people={listPeople}></List>
-    </Box>
+    <div>
+      <Box sx={{backgroundColor: 'secondary.main'}}>
+        <List people={listPeople}></List>
+      </Box>
+
+      <Dialog open={true} fullWidth PaperProps={{sx: {p: 5}}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label='Digite o nome'
+              type='text'
+              fullWidth
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label = 'Digite o email'
+              type='email'
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <DialogActions sx={{mt: 5}}>
+          <Button>Cancelar</Button>
+          <Button>Marcar</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   )
 }
 
