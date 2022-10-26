00a721d94055e0569f6269d1ce65d8d67bae93f0
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-26 12:16:26 -0300

    Delete testzin.py

--- sockets/testzin.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-file_name = 'horse_car.jpg'
-hasher = hashlib.md5()
-
-with open(file_name, 'rb') as file:
-    content = file.read()
-    hasher.update(content)
-print(hasher.hexdigest())
