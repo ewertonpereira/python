ef3d15c756d07e868fe58cd47aae54eea8e414be
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-10-26 10:46:41 -0300

    Update Client.py

--- sockets/client/Client.py
+++ sockets/client/Client.py
@@ -1,21 +1,33 @@
-from http import client
 import socket
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM ) # protocolo tcp
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM ) # protocolo tcp
 
-client.connect(('localhost', 7777))
+# client.connect(('localhost', 7777))
+# print('Conectado')
+
+# namefile = str(input('Arquivo>'))
+
+# client.send(namefile.encode())
+
+# with open(namefile, 'wb') as file:
+#     while 1:
+#         data = client.recv(1000000)
+#         if not data:
+#             break
+#         file.write(data)
+
+# print(f'{namefile} recebido"\n')
+
+HOST = '127.0.0.1'
+PORT = 50000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # DGRAM = UDP
+
+client.connect((HOST, 50000))
 print('Conectado')
 
-namefile = str(input('Arquivo>'))
+client.sendall(str.encode(input('Digite sua mensagem: ')))
 
-client.send(namefile.encode())
-
-with open(namefile, 'wb') as file:
-    while 1:
-        data = client.recv(1000000)
-        if not data:
-            break
-        file.write(data)
-
-print(f'{namefile} recebido"\n')
+data = client.recv(1024)
+print(f'Mensagem ecoada: {data.decode()}')
