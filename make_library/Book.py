ea2acb26c05318f4508a29ece8fed9da60c7fd07
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-11-06 00:24:21 -0300

    Create Book.py

---
+++ make_library/Book.py
@@ -0,0 +1,12 @@
+class Book:
+
+    def __ini__(self, author, title, year, weight):
+        self.id = id()
+        self.author = author
+        self.tile = title
+        self.year = year
+        self.weight = weight
+
+
+if __name__ == '__main__':
+    pass
