dce72ecc61cf78919a7d9afcab535ab6e68d39d7
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-11-07 00:11:01 -0300

    Update Book.py

--- make_library/Book.py
+++ make_library/Book.py
@@ -1,12 +1,18 @@
+from uuid import UUID, uuid4
+
+
 class Book:
 
-    def __ini__(self, author, title, year, weight):
-        self.id = id()
-        self.author = author
-        self.tile = title
-        self.year = year
-        self.weight = weight
+    def __init__(self, author: str, title: str, year: int, weight: float):
+        self.id: UUID = uuid4()
+        self.author: str = author
+        self.title: str = title
+        self.year: int = year
+        self.weight: float = weight
 
 
 if __name__ == '__main__':
     pass
+    # book = Book('harari', 'sapiens', 2010, 1.32)
+    # print(f'ID: {book.id}\nAutor: {book.author}\nTítulo: {book.title}\nAno: {book.year}\nPeso: {book.weight}')
+    # print(type(book.id))
