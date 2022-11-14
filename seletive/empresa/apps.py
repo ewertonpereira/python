a225bbe6bec2b3de319e4648381df912cfa1dfcb
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-11-14 12:34:56 -0300

    delete

--- seletive/empresa/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmpresaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'empresa'
--- seletive/empresa/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('nova_empresa/', views.nova_empresa, name='nova_empresa'),
-    
-]
