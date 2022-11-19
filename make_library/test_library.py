e22e2d17a103f85929ea0c0d91403d630e81311c
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-11-18 21:51:51 -0300

    Update test_library.py

--- make_library/test_library.py
+++ make_library/test_library.py
@@ -1,9 +1,24 @@
-from unittest import TestCase
+import unittest
+from Tools import get_author_book
 
 
-class TestAddAuthor(TestCase):
+class TestBookCase(unittest.TestCase):
     
     def setUp(self) -> None:
-        pass
+        self.get_author_book = get_author_book()
 
 
+    def test_get_author_book(self):
+        result = self.get_author_book
+        #print(result)
+        self.assertEqual(result, 'EWERTON')
+        
+        
+    # def test_spaces(self):
+    #     with self.assertRaises(Exception):
+    #         self.get_author_book('EWERTON  PEREIRA')
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
