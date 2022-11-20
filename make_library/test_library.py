f5c720b32544e9c0fd7842b2831ab5e1166f881e
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2022-11-20 15:01:54 -0300

    Update test_library.py

--- make_library/test_library.py
+++ make_library/test_library.py
@@ -1,20 +1,23 @@
-from unittest import mock, TestCase
-from tools import get_author_book
+# from unittest import mock, TestCase
+# from tools import get_author_book
 
 
-class TestBookCase(TestCase):
+# class TestBookCase(TestCase):
     
-    # @mock.patch('tools.get_author_book', return_value='ewerton')
-    # def test_get_author_book(self, author):
-    #     self.assertEqual(get_author_book(), 'ewerton')
+#     # @mock.patch('tools.get_author_book', return_value='ewerton')
+#     # def test_get_author_book(self, author):
+#     #     self.assertEqual(get_author_book(), 'ewerton')
         
-    @mock.patch('tools.get_author_book', side_effect=[
-        'ewerton',
-        'eduardo',
-        'kerlen'
-     ])
-    def test_get_author_book(self, author):
-        self.assertEqual(get_author_book(), 'ewerton')
-        self.assertEqual(get_author_book(), 'eduardo')
-        self.assertEqual(get_author_book(), 'kerlen')
-
+#     @mock.patch('tools.get_author_book', side_effect=[
+#         'ewerton',
+#         'eduardo',
+#         'kerlen'
+#      ])
+#     def test_get_author_book(self, author):
+#         self.assertEqual(get_author_book(), 'ewerton')
+#         self.assertEqual(get_author_book(), 'eduardo')
+#         self.assertEqual(get_author_book(), 'kerlen')
+        
+        
+# if __name__ == '__main__':
+#     unittest.main()
