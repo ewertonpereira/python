ad57082e865a7d44270328cc35cab86c2633f3f9
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2023-05-16 21:42:05 -0300

    update parser.py

--- test/parser.py
+++ test/parser.py
@@ -1,16 +1,39 @@
+from typing import List
 from lexer import read_code, lexicon, verify_tokens
 
 
 def analyze_code_with_verification(file_name, show_errors=True):
     if verify_tokens(file_name):
-        print("Verificação de tokens bem-sucedida.")
-        print("Começaremos o processo de analise sintática. Por favor, aguarde...")
+        print("Verificação de tokens bem-sucedida.\nComeçaremos o processo de análise sintática. Por favor, aguarde...")
         return True
     else:
-        if show_errors:
-            print("Erro: Token inválido encontrado.")
-            print("Verifique o arquivo selecionado e tente novamente.")
+        error_message = "Erro: Token inválido encontrado.\nVerifique o arquivo selecionado e tente novamente."
+        print(error_message) if show_errors else None
         return False
 
 
-analyze_code_with_verification('codigo.txt', show_errors=True)
+
+# analyze_code_with_verification('codigo.txt', show_errors=True)
+
+def read_token_values(file_name: str) -> List[str]:
+    with open(file_name, 'r') as file:
+        code = file.read().split()
+    
+    return code
+
+
+
+
+# print(token_values := read_token_values('codigo.txt'))
+def process_code_file(file_name: str) -> None:
+    if analyze_code_with_verification(file_name):
+        token_symbols = read_token_values(file_name)
+        print(token_symbols)
+    else:
+        print("Erro: O código não está de acordo com a gramática especificada.")
+
+
+
+process_code_file('codigo.txt')
+
+
