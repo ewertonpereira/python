81340a8f5b229aa3702f02e8c1c97559665180eb
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2023-06-20 16:20:23 -0300

    update delete get_code

--- test/parser.py
+++ test/parser.py
@@ -1,5 +1,5 @@
 from typing import List
-from lexer import verify_tokens, get_tokens_code
+from lexer import verify_tokens, get_tokens_code, lexicon_from_file
 
 
 def analyze_code_with_verification(file_name: str, show_errors: bool = True) -> bool:
@@ -9,24 +9,27 @@ def analyze_code_with_verification(file_name: str, show_errors: bool = True) -> 
     return False
     
 
-def parser(tokens: List[str]) -> None:
+def parser(tokens) ->  list[str]:
     token_index: int = 0
     token_count: int = len(tokens)
-    token: str = tokens[token_index]
+    
+    token_key = list(((key, value) for key, value in tokens
+    [token_index].items()))
     errors: List[str] = []
     matchs: List[str] = []
 
     
     def match(expected_token: str) -> None:
-        nonlocal token, token_index
-        if token == expected_token:
+        nonlocal token_index, token_key
+        if token_key[0][0] == expected_token:
             token_index += 1
             if token_index < token_count:
-                token = tokens[token_index]
+                token_key = list(((key, value) for key, value in tokens
+    [token_index].items()))
             else:
                 token = None
         else:
-            errors.append(f"Expected '{expected_token}', but found '{token}'")
+            errors.append(f"Expected '{expected_token}', but found '{token_key[0][1]}'")
             
 
     def analyze_list(token: str) -> None:
@@ -39,66 +42,66 @@ def parser(tokens: List[str]) -> None:
             'RBRACE': lambda: token,
         }
 
-        if token in actions:
-            actions[token]()
+        if token_key[0][0] in actions:
+            actions[token_key[0][0]]()
         else:
             errors.append(f'Variável inválida: {token}')
         
 
-    def check_id() -> bool:
-        if token == 'ID':
+    def check_id():
+        if token_key[0][0] == 'ID':
             match('ID')
             matchs.append('ID ok')
-            if token == 'COMMA':
+            if token_key[0][0] == 'COMMA':
                 match('COMMA')
                 check_id()
-            elif token == 'ATRIB':
+            elif token_key[0][0] == 'ATRIB':
                 match('ATRIB')
                 matchs.append('ATRIB ok')
-                if token == 'LPAREN':
+                if token_key[0][0] == 'LPAREN':
                     check_arithmetic_expressions()
-                elif token == 'ID' or token == 'NUM':
-                    match(token)
+                elif token_key[0][0] == 'ID' or token_key[0][0] == 'NUM':
+                    match(token_key[0][0])
                     check_basic_arithmetic_expressions()
-                    if token == 'NUM':
+                    if token_key[0][0] == 'NUM':
                         check_num()
-                    if token == 'ID':
+                    if token_key[0][0] == 'ID':
                         check_id()
                 else:
-                    errors.append(f'"ATRIB" expected a variable, but received {token}')
-            elif token in ['LT', 'GT', 'LTE', 'GTE', 'EQ']:
-                match(token)
-                if token == 'ID' or token == 'NUM':
+                    errors.append(f'"ATRIB" expected a variable, but received {token_key[0][1]}')
+            elif token_key[0][0] in ['LT', 'GT', 'LTE', 'GTE', 'EQ']:
+                match(token_key[0][0])               
+                if token_key[0][0] == 'ID' or token_key[0][0] == 'NUM':
                     matchs.append('Relational expressions ok')
-                    match(token)
+                    match(token_key[0][0])
                 else:
-                    errors.append(f"Relational expressions expected a variable, but received {token}")
+                    errors.append(f"Relational expressions expected a variable, but received {token_key[0][1]}")
                     exit()      
-            elif token in ['ADD', 'SUB', 'MUL', 'DIV', 'POW']:
-                match(token)
-                if token == 'ID' or token == 'NUM':
+            elif token_key[0][0] in ['ADD', 'SUB', 'MUL', 'DIV', 'POW']:
+                match(token_key[0][0])
+                if token_key[0][0] == 'ID' or token_key[0][0] == 'NUM':
                     matchs.append('Arithmetic expressions ok')
-                    match(token)
+                    match(token_key[0][0])
                 else:
-                    errors.append(f"Arithmetic expressions expected a variable, but received {token}")
-            elif token == 'RPAREN':
+                    errors.append(f"Arithmetic expressions expected a variable, but received {token_key[0][1]}")
+            elif token_key[0][0] == 'RPAREN':
                 check_arithmetic_expressions()
-            elif token in ['ATRIB_ADD', 'ATRIB_SUB']:
+            elif token_key[0][0] in ['ATRIB_ADD', 'ATRIB_SUB']:
                 matchs.append('combined assignment operator ok')
-                match(token)
-                if token == 'ID':
+                match(token_key[0][0])
+                if token_key[0][0] == 'ID':
                     check_id()
-                elif token == 'NUM':
+                elif token_key[0][0] == 'NUM':
                     check_num()
                 else:
-                    errors.append(f'Expected "ID" or "NUM", but received {token}'   )
-            if token == 'SEMICOLON':
+                    errors.append(f'Expected "ID" or "NUM", but received {token_key[0][1]}'   )
+            if token_key[0][0] == 'SEMICOLON':
                 check_semicolon()
                 return True
             else:
                 return False
         else:
-            errors.append(f'Expected "ID", but received {token}')
+            errors.append(f'Expected "ID", but received {token_key[0][1]}')
             
 
     def check_semicolon() -> None:
@@ -111,85 +114,85 @@ def parser(tokens: List[str]) -> None:
     def check_while() -> bool:
         match('WHILE')
         matchs.append('WHILE ok')
-        if token == 'LPAREN':
+        if token_key[0][0] == 'LPAREN':
             match('LPAREN')
             matchs.append('LPAREN ok')
-            if token == 'ID':
+            if token_key[0][0] == 'ID':
                 check_id()
-                if token == 'RPAREN':
+                if token_key[0][0] == 'RPAREN':
                     match('RPAREN')
                     matchs.append('RPAREN ok')
-                    if token == 'LBRACE':
+                    if token_key[0][0] == 'LBRACE':
                         match('LBRACE')
                         matchs.append('LBRACE ok')
-                        if token == 'ID':
+                        if token_key[0][0] == 'ID':
                             check_id()
-                            if token == 'IF':
+                            if token_key[0][0] == 'IF':
                                 check_if()
-                                analyze_list(token)
-                                if token == 'ELSE':
+                                analyze_list(token_key[0][0])
+                                if token_key[0][0] == 'ELSE':
                                     check_else()
-                                analyze_list(token)
-                        if token == 'RBRACE':
+                                analyze_list(token_key[0][0])
+                        if token_key[0][0] == 'RBRACE':
                             matchs.append('RBRACE WHILE ok')
                             if token_index + 1 < token_count:
                                 match('RBRACE')
-                            analyze_list(token)                          
+                            analyze_list(token_key[0][0])                          
                 else:
                     errors.append(
-                        f'Expected "ID" or "RPAREN 1", but received {token}')
+                        f'Expected "ID" or ")", but received {token_key[0][1]}')
             else:
                 errors.append(
-                    f'Expected "ID" or "RPAREN" 2, but received {token}')
+                    f'Expected "ID" or ")", but received {token_key[0][1]}')
         return True
 
 
     def check_if() -> None:
         match('IF')
         matchs.append('IF ok')
-        if token == 'LPAREN':
+        if token_key[0][0] == 'LPAREN':
             match('LPAREN')
             matchs.append('LPAREN ok')
             check_id()
-            if token == 'RPAREN':
+            if token_key[0][0] == 'RPAREN':
                 match('RPAREN')
                 matchs.append('RPAREN ok')
-                if token == 'LBRACE':
+                if token_key[0][0] == 'LBRACE':
                     matchs.append('LBRACE ok')
                     match('LBRACE')
                     check_id()
-                    if token == 'RBRACE':
+                    if token_key[0][0] == 'RBRACE':
                         matchs.append('RBRACE  ok')
                         if token_index + 1 < token_count:
                             match('RBRACE')
-                            analyze_list(token)
+                            analyze_list(token_key[0][0])
                     else:
                         errors.append(
-                            f'Expected "RBRACE", but received {token}')
+                            f'Expected "}}", but received {token_key[0][1]}')
                 else:
-                    errors.append(f'Expected "LBRACE", but received {token}')
+                    errors.append(f'Expected "{{", but received {token_key[0][1]}')
             else:
-                errors.append(f'Expected "RPAREN", but received {token}')
+                errors.append(f'Expected ")", but received {token_key[0][1]}')
         else:
-            errors.append(f'Expected "LPAREN", but received {token}')
+            errors.append(f'Expected "(", but received {token_key[0][1]}')
 
 
     def check_else() -> None:
         match('ELSE')
         matchs.append('ELSE ok')
-        if token == 'LBRACE':
+        if token_key[0][0] == 'LBRACE':
             match('LBRACE')
             matchs.append('LBRACE ok')
             check_id()
-            if token == 'RBRACE':
+            if token_key[0][0] == 'RBRACE':
                 matchs.append('RBRACE ELSE ok')
                 if token_index + 1 < token_count:
                     match('RBRACE')
-                    analyze_list(token)
+                    analyze_list(token_key[0][0])
             else:
-                errors.append(f'Expected "RBRACE", but received {token}')
+                errors.append(f'Expected "}}", but received {token_key[0][1]}')
         else:
-            errors.append(f'Expected "LBRACE", but received {token}')
+            errors.append(f'Expected "{{", but received {token_key[0][1]}')
 
 
     def check_num() -> None:
@@ -200,51 +203,51 @@ def parser(tokens: List[str]) -> None:
         
 
     def check_basic_arithmetic_expressions() -> None:
-        if token in ['ADD', 'SUB', 'MUL', 'DIV']:
-            match(token)
+        if token_key[0][0] in ['ADD', 'SUB', 'MUL', 'DIV']:
+            match(token_key[0][0])
             matchs.append('Expressões aritméticas ok')
-        elif token == 'POW':
+        elif token_key[0][0] == 'POW':
             match('POW')
-            if token == 'ID' or token == 'NUM':
-                match(token)
+            if token_key[0][0] == 'ID' or token_key[0][0] == 'NUM':
+                match(token_key[0][0])
                 check_basic_arithmetic_expressions()
 
     
     def check_arithmetic_expressions() -> None:
-        if token == 'LPAREN':
+        if token_key[0][0] == 'LPAREN':
             match('LPAREN')
             matchs.append('LPAREN ok')
             check_arithmetic_expressions()
-            if token == 'RPAREN':
+            if token_key[0][0] == 'RPAREN':
                 match('RPAREN')
                 matchs.append('RPAREN CLOSED ok')
                 if token_index + 1 < token_count:
                     check_basic_arithmetic_expressions()
                     check_arithmetic_expressions()
             else:
-                errors.append(f'Expected "RPAREN", but received {token}')
-        elif token == 'NUM':
+                errors.append(f'Expected ")", but received {token_key[0][1]}')
+        elif token_key[0][0] == 'NUM':
             matchs.append('NUM ok')
             match('NUM')
             check_basic_arithmetic_expressions()
             check_arithmetic_expressions()
-        elif token == 'ID':
+        elif token_key[0][0] == 'ID':
             check_id()
-        elif token == 'LPAREN':
+        elif token_key[0][0] == 'LPAREN':
             matchs.append('LPAREN ok')
 
 
     def check_relational_expressions() -> None:
-        if token in ['LT', 'GT', 'LTE', 'GTE', 'EQ']:
+        if token_key[0][0] in ['LT', 'GT', 'LTE', 'GTE', 'EQ']:
             matchs.append('Relational expressions ok')
-            match(token)
+            match(token_key[0][0])
 
 
     def check_var() -> None:
         match('VAR')
         matchs.append('VAR ok')
-        if token in ['INT', 'STR', 'REAL', 'BOOL']:
-            match(token)
+        if token_key[0][0] in ['INT', 'STR', 'REAL', 'BOOL']:
+            match(token_key[0][0])
             check_id()
 
 
@@ -256,24 +259,24 @@ def parser(tokens: List[str]) -> None:
 
 
     while token_index < token_count:
-        if token == None:
+        if token_key[0][0] == None:
             break
-        elif token == 'ID':
+        elif token_key[0][0] == 'ID':
             check_id()
-        elif token == 'VAR':
+        elif token_key[0][0] == 'VAR':
             check_var()
-        elif token == 'IF':
+        elif token_key[0][0] == 'IF':
             check_if()
-        elif token == 'ELSE':
+        elif token_key[0][0] == 'ELSE':
             check_else()
-        elif token == 'WHILE':
+        elif token_key[0][0] == 'WHILE':
             check_while()
-        elif token not in ['VAR', 'ID', 'IF', 'WHILE']:
+        elif token_key[0][0] not in ['VAR', 'ID', 'IF', 'WHILE']:
             if not matchs:
-                errors.append(f"This token can not start a sentence: {token}")
+                errors.append(f"This token can not start a sentence: {token_key[0][1]}")
             break
         else:
-            errors.append(f"Invalid token: {token}")
+            errors.append(f"Invalid token: {token_key[0][1]}")
         
 
     if matchs:
@@ -284,7 +287,8 @@ def parser(tokens: List[str]) -> None:
 
 def analyze_code(file_name: str) -> None:
     if analyze_code_with_verification(file_name):
-        token_symbols = get_tokens_code(file_name)
+        #token_symbols = get_tokens_code(file_name)
+        token_symbols = list(lexicon_from_file(file_name))
         # print(token_symbols)
         errors = parser(token_symbols)
         if not errors:
